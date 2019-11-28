daf25244adde8e1ca8ef909d3f679090a2877b34
Stan Yaro <skyterskyter@gmail.com>
2019-11-28 22:43:05 +0200

    actions creator and type

--- src/Redux/state.js
+++ src/Redux/state.js
@@ -1,3 +1,6 @@
+const ADD_POST = 'ADD-POST';
+const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
+
 let store = {
   _state : {
     profilePage: {
@@ -37,10 +40,10 @@ let store = {
   },
   subscribe(observer) {
     this._callSubscriber = observer;
-  },
+  }, 
   
   dispatch(action) {
-    if (action.type === 'ADD-POST') {
+    if (action.type === ADD_POST) {
       let newPost = {
         id: 5,
         message: this._state.profilePage.newPostText,
@@ -55,6 +58,10 @@ let store = {
     }
   }
 }
+
+export const addPostActionCreator = () => ({ type: ADD_POST })
+export const updateNewPostActionCreator = (text) => ({ type: UPDATE_NEW_POST_TEXT, newText: text })
+
 
 export default store;
 window.store = store;
--- src/components/Profile/My posts/MyPosts.jsx
+++ src/components/Profile/My posts/MyPosts.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import s from './MyPosts.module.css';
 import Post from './Post/Post';
-
+import { addPostActionCreator, updateNewPostActionCreator } from '../../../Redux/state';
 
 const MyPosts = (props) => {
 let postsElements = props.posts.map ( post => <Post message={post.message} likesCount={post.likesCount} />);
@@ -10,12 +10,14 @@ let newPostElement = React.createRef();
 
 
 let addPost = () => {
-  props.dispatch({type: 'ADD-POST'});
+  props.dispatch(addPostActionCreator());
 }
 
 let onPostChange = () => {
   let text = newPostElement.current.value;
-  props.dispatch({type: 'UPDATE-NEW-POST-TEXT', newText: text});
+  // props.dispatch({type: 'UPDATE-NEW-POST-TEXT', newText: text});
+  let action = updateNewPostActionCreator(text);
+  props.dispatch(action);
 }
  
     return (
