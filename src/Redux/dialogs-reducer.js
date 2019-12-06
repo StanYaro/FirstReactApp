cb8e7159fc1c3eacf13543b32adaf5e00f892d1e
Stan Yaro <skyterskyter@gmail.com>
2019-12-06 20:26:43 +0200

    dialogs-reducer copy

--- src/Redux/dialogs-reducer.js
+++ src/Redux/dialogs-reducer.js
@@ -22,15 +22,24 @@ let initialState = {
 };
 
 const dialogsReducer = (state = initialState, action) => {
+
+  let stateCopy;
     switch(action.type) {
         case UPDATE_NEW_MESSAGE_BODY:
-          state.newMessageBody = action.body;
-          return state;
-        case SEND_MESSAGE:
-          let body =state.newMessageBody;
-          state.newMessageBody = '';
-          state.messages.push({id:6, message: body});
-          return state;
+        stateCopy = {
+          ...state,
+          newMessageBody: action.body
+        };
+
+          return stateCopy;
+      case SEND_MESSAGE:
+        let body = state.newMessageBody;
+        stateCopy = {
+          ...state,
+          newMessageBody: '',
+          messages: [...state.messages, {id:6, message: body}]
+      };
+          return stateCopy;
         default: 
           return state;
     }
