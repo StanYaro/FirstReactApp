c292d2cfb75dc6aca431c062a00535ab843ffa15
Stan Yaro <skyterskyter@gmail.com>
2019-12-10 21:06:38 +0200

    axios part 2 -pure function plus button

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,28 +1,23 @@
 import React from 'react';
 import styles from './Users.module.css';
 import * as axios from 'axios';
+import userPhoto from '../../assets/img/user.jpg';
 
 let Users = (props) => {
-    if (props.users.length === 0) {
-
-        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(Response => {
-            props.setUsers (Response) 
+    let getUsers = () => {
+        if (props.users.length === 0) {
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+            props.setUsers (response.data.items) 
         });
-
-    //     props.setUsers ([
-    //     {id: 1, photoUrl:'https://i.pinimg.com/originals/e0/73/cf/e073cff3ef3e0e5f173598208a1a0dee.jpg', followed: false, fullName: 'Stanislav', status: 'I am a Junior!', location:{city: 'Chisinau', country: 'Moldova'} },
-    //     {id: 2, photoUrl:'https://i.pinimg.com/originals/14/c7/2c/14c72ce84186246df41b9bf943932d1d.jpg', followed: true, fullName: 'Alexander', status: 'I am a Senior!', location:{city: 'London', country: 'Britain'} },
-    //     {id: 3, photoUrl:'https://mir-s3-cdn-cf.behance.net/project_modules/1400/3671da38650505.598fa119575fb.jpg', followed: true, fullName: 'Nadin', status: 'I am a Wife', location:{city: 'Moscow', country: 'Russia'} },
-    //     {id: 4, photoUrl:'https://i.kym-cdn.com/photos/images/original/000/357/581/514.gif', followed: false, fullName: 'Kubik', status: 'I am dump-cat', location:{city: 'Street', country: 'NoWhere'} },    
-    //   ])      
-    }
-    
-    return <div>     
+    } 
+}    
+    return <div> 
+        <button onClick={getUsers}>Get Users</button>    
         {
             props.users.map( u => <div key={u.id}>
                 <span>
                     <div>
-                        <img src={u.photoUrl} className={styles.userPhoto}/>
+                        <img src={u.photos.small != null ? u.photos.small : userPhoto } className={styles.userPhoto}/>
                     </div>
                     <div>
                         {u.followed 
@@ -33,12 +28,12 @@ let Users = (props) => {
 
                 <span>
                     <span>
-                        <div>{u.fullName}</div>
+                        <div>{u.name}</div>
                         <div>{u.status}</div>
                     </span>
                     <span>
-                        <div>{u.location.country}</div>
-                        <div>{u.location.city}</div>
+                        <div>{"u.location.country"}</div>
+                        <div>{"u.location.city"}</div>
                     </span>
                 </span>
             </div>)
