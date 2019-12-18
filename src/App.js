6a596f8390694204c18cbdeab0b141e9e1e834c0
Stan Yaro <skyterskyter@gmail.com>
2019-12-18 14:05:27 +0200

    login auth me

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import React from 'react';
 import './App.css';
 import {BrowserRouter, Route} from 'react-router-dom';
-import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
-import Dialogs from './components/Dialogs/Dialogs';
 import News from './components/News/News';
 import Music from './components/Music/Music';
 import Setings from './components/Setings/Setings';
 import DialogsContainer from './components/Dialogs/DialogsContainer';
 import UsersContainer from './components/Users/UsersContainer';
 import ProfileContainer from './components/Profile/ProfileContainer';
+import HeaderContainer from './components/Header/HeaderContainer';
 
 
 
 const App = (props) => {
     return (
     <div className='app-wrapper'>
-      <Header />
+      <HeaderContainer />
       <Navbar />
       <div className='app-wrapper-content'>
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import s from './Header.module.css';
+import {NavLink} from 'react-router-dom';
 
-const Header = () => {
+const Header = (props) => {
     return (
         <header className={s.header}>
-        <img src='https://i.pinimg.com/736x/93/f3/71/93f371f7ca6577d476317b47bc645243--pumpkin-template-printable-pumpkin-stencils.jpg'></img>
+            <img src='https://i.pinimg.com/736x/93/f3/71/93f371f7ca6577d476317b47bc645243--pumpkin-template-printable-pumpkin-stencils.jpg'></img>
+        
         <span className={s.logoName}><h1>Avengersbook</h1></span>
+
+        <div className={s.loginBlock}>
+        {props.isAuth ? props.login
+                      : <NavLink to={'/login'}>Login</NavLink> 
+        }        
+        </div>
      </header>
     );
 }
