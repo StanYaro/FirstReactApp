9af83daf8e36a9305241e28a1163763faf0881a0
Stan Yaro <skyterskyter@gmail.com>
2019-12-28 15:52:04 +0200

    Thunk refactoring header/profile

--- src/components/Header/HeaderContainer.jsx
+++ src/components/Header/HeaderContainer.jsx
@@ -1,28 +1,21 @@
 import React from 'react';
 import Header from './Header';
 import { connect } from 'react-redux';
-import {setAuthUserData} from '../../Redux/auth-reducer';
-import { authAPI } from '../../api/api';
+import {getAuthUserData} from '../../Redux/auth-reducer';
 
 class HeaderContainer extends React.Component {
 
     componentDidMount() {
-        authAPI.me().then(response => {            
-                if (response.data.resultCode === 0) {
-                    let {id, login, email} = response.data.data;
-                    this.props.setAuthUserData(id, email, login);                    
-                }
-            });
+        this.props.getAuthUserData();
     }
-
     render () {
-    return (
-        <Header {...this.props} />
-    )}
+        return (
+            <Header {...this.props} />
+        )}
 }
 
 const mapStateToProps = (state) => ({
     isAuth: state.auth.isAuth,
     login: state.auth.login,
 });
-export default connect(mapStateToProps, {setAuthUserData})(HeaderContainer);
+export default connect(mapStateToProps, {getAuthUserData})(HeaderContainer);
--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import Profile from './Profile';
 import {connect} from 'react-redux';
-import {setUserProfile} from '../../Redux/profile-reducer';
+import {getUserProfile} from '../../Redux/profile-reducer';
 import {withRouter} from 'react-router-dom';
-import { usersAPI } from '../../api/api';
 
 class ProfileContainer extends React.Component {
 
@@ -11,10 +10,8 @@ class ProfileContainer extends React.Component {
         let userId = this.props.match.params.userId;
         if (!userId) {
             userId = 2;
-        }
-        usersAPI.getProfile(userId).then(response => {                    
-                this.props.setUserProfile (response.data);
-            });    
+        }   
+        this.props.getUserProfile(userId);       
     }
 
     render() 
@@ -29,4 +26,4 @@ let mapStateToProps = (state) => ({
 
 let WithUrlDataContainerComponent = withRouter(ProfileContainer);
 
-export default connect (mapStateToProps, {setUserProfile}) (WithUrlDataContainerComponent);
+export default connect (mapStateToProps, {getUserProfile}) (WithUrlDataContainerComponent);
