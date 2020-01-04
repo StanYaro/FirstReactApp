969ec320ea071511f3734725025deaef625ca4a1
Stan Yaro <skyterskyter@gmail.com>
2020-01-04 16:02:20 +0200

    status for profile with setState

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import s from './ProfileInfo.module.css';
 import Preloader from '../../common/Preloader/preloader';
+import ProfileStatus from './ProfileStatus'
 
 const ProfileInfo = (props) => {
     if (!props.profile) {
@@ -8,13 +9,13 @@ const ProfileInfo = (props) => {
     }    
     return (
     <div>
-        <div>
+        {/* <div>
          <img src='https://www.zastavki.com/pictures/originals/2014/Nature___Islands_Island_in_the_tropics_089496_.jpg' className={s.contentback}></img> 
-        </div>
+        </div> */}
 
         <div  className={s.descriptionBlock} > 
             <img src={props.profile.photos.large} />
-            ava + description
+             <ProfileStatus  status={"Hello status!"} />
         </div> 
 
     </div>
