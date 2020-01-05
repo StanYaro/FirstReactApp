3ee844fd77c9b1a4bc1d045264ffb193c77e02a6
Stan Yaro <skyterskyter@gmail.com>
2020-01-05 10:10:03 +0200

    refactoring status with compose

--- src/Redux/profile-reducer.js
+++ src/Redux/profile-reducer.js
@@ -1,8 +1,10 @@
-import { usersAPI } from "../api/api";
+import { usersAPI, profileAPI } from "../api/api";
 
 const ADD_POST = 'ADD-POST';
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
 const SET_USER_PROFILE = 'SET_USER_PROFILE';
+const SET_STATUS = 'SET_STATUS';
+
 
 
 let initialState = {
@@ -13,7 +15,8 @@ let initialState = {
     {id: '4', message: 'asgasdgasdgasd', likesCount: 3}
   ],
   newPostText: 'React is the best!',
-  profile: null
+  profile: null,
+  status: ""
 };
 
 const profileReducer = (state = initialState, action) => {
@@ -38,6 +41,12 @@ const profileReducer = (state = initialState, action) => {
             newPostText: action.newText         
           }
         }
+        case SET_STATUS: {
+          return {
+            ...state,   
+            status: action.status         
+          }
+        }
         case SET_USER_PROFILE: {
           return {
             ...state,   
@@ -51,11 +60,26 @@ const profileReducer = (state = initialState, action) => {
 
 export const addPostActionCreator = () => ({ type: ADD_POST })
 export const setUserProfile = (profile) => ({ type: SET_USER_PROFILE, profile })
+export const setStatus = (status) => ({ type: SET_STATUS, status })
 
 export const getUserProfile = (userId) => (dispatch) => {
   usersAPI.getProfile(userId).then(response => {                    
     dispatch(setUserProfile (response.data));
 });  
+} 
+
+export const getStatus = (userId) => (dispatch) => {
+  profileAPI.getStatus(userId).then(response => {                    
+    dispatch(setStatus (response.data));
+});  
+} 
+
+export const updateStatus = (status) => (dispatch) => {
+  profileAPI.updateStatus(status).then(response => {
+    if (response.data.resultCode === 0) {                   
+      dispatch(setStatus (response.data));
+    }
+  });  
 } 
 
 export const updateNewPostActionCreator = (text) => ({ type: UPDATE_NEW_POST_TEXT, newText: text })
--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -15,7 +15,7 @@ const ProfileInfo = (props) => {
 
         <div  className={s.descriptionBlock} > 
             <img src={props.profile.photos.large} />
-             <ProfileStatus  status={"Hello status!"} />
+            <ProfileStatus  status={props.status} updateStatus={props.updateStatus} />
         </div> 
 
     </div>
